c1161f120b89832a3285777f255847848917617e
Thomas <thomas510111@gmail.com>
2016-01-04 18:19:14 +0200

    Changed the table

--- db.sql
+++ db.sql
@@ -1,43 +1,65 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb2+deb7u1
+-- version 4.4.15deb1
 -- http://www.phpmyadmin.net
 --
--- Φιλοξενητής: localhost
--- Χρόνος δημιουργίας: 15 Μαρ 2015 στις 14:54:37
--- Έκδοση διακομιστή: 5.5.41
--- Έκδοση PHP: 5.4.36-0+deb7u3
+-- Host: localhost
+-- Generation Time: Jan 04, 2016 at 06:18 PM
+-- Server version: 5.6.27-2
+-- PHP Version: 5.6.16-2
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Βάση: `Refrigerator`
+-- Database: `productdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Δομή πίνακα για τον πίνακα `Items`
+-- Table structure for table `products`
 --
 
-CREATE TABLE IF NOT EXISTS `Items` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
-  `barcode` bigint(12) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `type` varchar(32) NOT NULL DEFAULT 'none',
-  `quantity` int(12) NOT NULL,
-  `expire_date` date NOT NULL DEFAULT '0000-00-00',
-  `insert_date` datetime NOT NULL,
-  `status` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`,`barcode`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(5) NOT NULL,
+  `barcode` int(32) DEFAULT NULL,
+  `title` varchar(16) DEFAULT NULL,
+  `image` int(5) DEFAULT NULL,
+  `productid` int(5) DEFAULT NULL,
+  `description` text,
+  `category` int(5) DEFAULT NULL,
+  `country` varchar(16) DEFAULT NULL,
+  `quantity` int(5) DEFAULT NULL,
+  `unit` varchar(8) DEFAULT NULL,
+  `user` int(5) DEFAULT NULL,
+  `insert_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
